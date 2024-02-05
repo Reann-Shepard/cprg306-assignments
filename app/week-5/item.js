630b6ad7037173243dc5a972888a6c4f3a206f02
Linden <tykor.wulfen@gmail.com>
2024-02-05 13:29:19 -0700

    Initial Week 5
    Uploading initial state of the week 5 assignment

---
+++ app/week-5/item.js
@@ -0,0 +1,17 @@
+// Create functional component named Item
+// Should contain name, quantity, category as props and display them in a list item element
+// Use Tailwind classes for styling
+
+export default function Item({id, name, quantity, category}) {
+    // Add functionality that allows user to click on item to mark it as purchased
+    // Hint: use state to track whether item has been purchased or not
+    
+    return(
+        <div className="mx-4 my-4 p-2 w-96 bg-slate-900">
+            <h2 className="text-xl font-bold">{name}</h2>
+            <p className="text-sm">
+                Buy {quantity} in {category}
+            </p>
+         </div>
+    );
+}
